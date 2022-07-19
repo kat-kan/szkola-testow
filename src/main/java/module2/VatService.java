9a7855e488bba6dd4d1fdf5bf3894204de88e0a7
Oregano <kankowska.katarzyna@gmail.com>
2022-07-19 08:53:40 +0200

    Use VatProvider in VatService. Adjust unit tests
    * Add VatProvider as source of vatValue in VatService. Add new method that generates gross price for product type
    * Adjust unit tests. Stub VatProvider answers
    * Add Mockito dependency in pom.xml

--- src/main/java/module2/VatService.java
+++ src/main/java/module2/VatService.java
@@ -4,18 +4,22 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class VatService {
-    private BigDecimal vatValue;
+    private VatProvider vatProvider;
 
-    public VatService() {
-        this.vatValue = new BigDecimal("0.23");
+    public VatService(VatProvider vatProvider) {
+        this.vatProvider = vatProvider;
     }
 
     public BigDecimal getGrossPriceForDefaultVat(Product product) {
 
-        return getGrossPrice(product.getNetPrice(), vatValue).setScale(2, RoundingMode.CEILING);
+        return calculateGrossPrice(product.getNetPrice(), vatProvider.getDefaultVat()).setScale(2, RoundingMode.CEILING);
     }
 
-    public BigDecimal getGrossPrice(BigDecimal netPrice, BigDecimal vatValue) {
+    public BigDecimal getGrossPriceForVatSpecificToType(Product product){
+        return calculateGrossPrice(product.getNetPrice(), vatProvider.getVatForType(product.getType()));
+    }
+
+    public BigDecimal calculateGrossPrice(BigDecimal netPrice, BigDecimal vatValue) {
         if (vatValue.compareTo(BigDecimal.ONE) == 1) {
             throw new IncorrectVatValueException("Podana wartość VAT jest wyższa od 1 (100% ceny produktu)");
         }
--- src/test/java/module2/VatServiceTest.java
+++ src/test/java/module2/VatServiceTest.java
@@ -3,19 +3,23 @@ package module2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.when;
 
 class VatServiceTest {
     private VatService vatService;
+    private VatProvider vatProvider;
 
     @BeforeEach
     void createVatService() {
-        vatService = new VatService();
+        vatProvider = Mockito.mock(VatProvider.class);
+        vatService = new VatService(vatProvider);
     }
 
     @Test
@@ -24,6 +28,7 @@ class VatServiceTest {
         //given
         String type = "Firewood";
         Product product = getProduct("20.0", type);
+        when(vatProvider.getDefaultVat()).thenReturn(new BigDecimal("0.23"));
 
         //when
         BigDecimal result = vatService.getGrossPriceForDefaultVat(product);
@@ -39,9 +44,10 @@ class VatServiceTest {
         String type = "Fertilizer";
         Product product = getProduct("10.0", type);
         BigDecimal vatValue = new BigDecimal("0.08");
+        stubGetVatForType(type, vatValue);
 
         //when
-        BigDecimal result = vatService.getGrossPrice(product.getNetPrice(), vatValue);
+        BigDecimal result = vatService.getGrossPriceForVatSpecificToType(product);
 
         //then
         assertThat(result).isEqualTo(new BigDecimal("10.80"));
@@ -54,9 +60,10 @@ class VatServiceTest {
         String type = "Bread";
         Product product = getProduct("100.90", type);
         BigDecimal vatValue = BigDecimal.ZERO;
+        stubGetVatForType(type, vatValue);
 
         //when
-        BigDecimal result = vatService.getGrossPrice(product.getNetPrice(), vatValue);
+        BigDecimal result = vatService.getGrossPriceForVatSpecificToType(product);
 
         //then
         assertThat(result).isEqualTo(new BigDecimal("100.90"));
@@ -69,12 +76,17 @@ class VatServiceTest {
         String type = "Toys";
         Product product = getProduct("30.0", type);
         BigDecimal vatValue = BigDecimal.TEN;
+        stubGetVatForType(type, vatValue);
 
         //then
-        assertThatExceptionOfType(IncorrectVatValueException.class).isThrownBy(() -> vatService.getGrossPrice(product.getNetPrice(), vatValue));
+        assertThatExceptionOfType(IncorrectVatValueException.class).isThrownBy(() -> vatService.getGrossPriceForVatSpecificToType(product));
     }
 
     private Product getProduct(String price, String type) {
         return new Product(UUID.randomUUID(), new BigDecimal(price), type);
+    }
+
+    private void stubGetVatForType(String type, BigDecimal vatValue) {
+        when(vatProvider.getVatForType(type)).thenReturn(vatValue);
     }
 }
