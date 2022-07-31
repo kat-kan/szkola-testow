59713747aa48ca094ce4d2188184ce5d285945af
Oregano <kankowska.katarzyna@gmail.com>
2022-07-31 12:40:17 +0200

    Add functionality to return list of adult employees

--- src/main/java/module3volunteers/EmployeeReportService.java
+++ src/main/java/module3volunteers/EmployeeReportService.java
@@ -1,5 +1,12 @@
 package module3volunteers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeeReportService {
 
+    public List<Employee> getAdultEmployees() {
+        List<Employee> employees = new ArrayList<>();
+        return employees;
+    }
 }
--- src/test/java/module3volunteers/EmployeeReportServiceTest.java
+++ src/test/java/module3volunteers/EmployeeReportServiceTest.java
@@ -1,9 +1,33 @@
 package module3volunteers;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class EmployeeReportServiceTest {
+    private EmployeeReportService employeeReportService;
 
+    @BeforeEach
+    void setUp() {
+        employeeReportService = new EmployeeReportService();
+    }
 
-/*    public static final int ADULT_AGE = 18;
+    @Test
+    @DisplayName("Should return list of employees")
+    void shouldReturnListOfEmployees() {
+        //when
+        List<Employee> adultEmployees = employeeReportService.getAdultEmployees();
+
+        //then
+        Assertions.assertThat(adultEmployees)
+                .isNotNull();
+
+    }
+
+    /*    public static final int ADULT_AGE = 18;
 
     @Test
     @DisplayName("Should return list of employees who are 18 or older")
