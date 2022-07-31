35148145ba0cf1b29deba34e3a735f6ea68ce863
Oregano <kankowska.katarzyna@gmail.com>
2022-07-31 12:53:18 +0200

    Add the actual employees to the report

--- src/main/java/module3volunteers/EmployeeReportService.java
+++ src/main/java/module3volunteers/EmployeeReportService.java
@@ -1,12 +1,17 @@
 package module3volunteers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeReportService {
 
+    private final List<Employee> employees;
+
+    public EmployeeReportService(List<Employee> employees) {
+
+        this.employees = employees;
+    }
+
     public List<Employee> getAdultEmployees() {
-        List<Employee> employees = new ArrayList<>();
         return employees;
     }
 }
--- src/test/java/module3volunteers/EmployeeReportServiceTest.java
+++ src/test/java/module3volunteers/EmployeeReportServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeReportServiceTest {
@@ -12,7 +13,7 @@ public class EmployeeReportServiceTest {
 
     @BeforeEach
     void setUp() {
-        employeeReportService = new EmployeeReportService();
+        employeeReportService = new EmployeeReportService(List.of());
     }
 
     @Test
@@ -25,6 +26,21 @@ public class EmployeeReportServiceTest {
         Assertions.assertThat(adultEmployees)
                 .isNotNull();
 
+    }
+
+    @Test
+    @DisplayName("Should return grocery store employees")
+    void shouldReturnGroceryStoreEmployees() {
+        //given
+        List<Employee> employees = List.of(new Employee("Jan Kowalski", 17), new Employee("Barbara Kowalska", 40));
+        employeeReportService = new EmployeeReportService(employees);
+
+        //when
+        List<Employee> adultEmployees = employeeReportService.getAdultEmployees();
+
+        //then
+        Assertions.assertThat(adultEmployees)
+                .isNotEmpty();
     }
 
     /*    public static final int ADULT_AGE = 18;
